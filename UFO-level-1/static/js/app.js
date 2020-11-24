2060a2ace1543840363dcb638620fdbf48d44934
matthewbelevski <68624883+matthewbelevski@users.noreply.github.com>
2020-11-25 10:13:42 +1100

    Uploading code that filters and updates table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,27 +2,28 @@
 var tableData = data;
 
 // YOUR CODE HERE!
-
+//selects the tbody of the table
 var tbody = d3.select("tbody");
 
+//appends the data to the table
 var originalTable = tableData.forEach(function(aliens) {
   console.log(aliens);
   var row = tbody.append("tr");
   Object.entries(aliens).forEach(function([key, value]) {
-    //console.log(key, value);
-    // Append a cell to the row for each value
-    // in the weather report object
+
+    // Append a cell to the row for each value in the data
     var cell = row.append("td");
     cell.text(value);
   });
 });
 
-
+ //selects the button
 var button = d3.select("#filter-btn");
 
+//selects the form
 var form = d3.select("#datetime");
 
-
+// Create event handlers
 button.on("click", runEnter);
 form.on("submit",runEnter);
 
@@ -38,16 +39,17 @@ function runEnter() {
     var inputValue = inputElement.property("value");
   
     console.log(inputValue);
-    //console.log(tableData);
+    
   
+  //filters the data based on the datetime key
     var filteredData = tableData.filter(alien => alien.datetime === inputValue);
   
     console.log(filteredData);
   
-    //var ages = filteredData.map(alien => alien.age);
-
+    //clears the original table while keeping the header row
+    var bodyRef = document.getElementById('ufo-table').getElementsByTagName('tbody')[0]; bodyRef.innerHTML = '';
    
-
+    //appends filtered data to the cleared table
     filteredData.forEach((aliens) => {
         var row = tbody.append("tr");
         Object.entries(aliens).forEach(([key, value]) => {
@@ -56,5 +58,4 @@ function runEnter() {
         });
       });
       
-  
 };
