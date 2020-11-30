95b49dd75b1e9f5ef8f766a2279d5c8b7fc191b2
matthewbelevski <68624883+matthewbelevski@users.noreply.github.com>
2020-11-30 11:59:20 +1100

    might be on the right track for the multi filter

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,6 +1,7 @@
 // from data.js
 var tableData = data;
 
+
 // YOUR CODE HERE!
 //selects the tbody of the table
 var tbody = d3.select("tbody");
@@ -30,31 +31,6 @@ var input = document.getElementById("datetime");
 //button.on("click", runEnter);
 //form.on("submit", event);
 
-function empty(data)
-{
-  if(typeof(data) == 'number' || typeof(data) == 'boolean')
-  { 
-    return false; 
-  }
-  if(typeof(data) == 'undefined' || data === null)
-  {
-    return true; 
-  }
-  if(typeof(data.length) != 'undefined')
-  {
-    return data.length == 0;
-  }
-  var count = 0;
-  for(var i in data)
-  {
-    if(data.hasOwnProperty(i))
-    {
-      count ++;
-    }
-  }
-  return count == 0;
-}
-
 
 input.addEventListener("keyup", function(event) {
 
@@ -62,7 +38,10 @@ input.addEventListener("keyup", function(event) {
 
      // Prevent the page from refreshing
      event.preventDefault();
+
+     var searchedData = {};
     
+
       // Select the input elements and get the raw HTML node
       var inputElementDate = d3.select("#datetime");
       var inputElementCity = d3.select("#city");
@@ -71,79 +50,47 @@ input.addEventListener("keyup", function(event) {
       var inputElementShape = d3.select("#shape");
     
       // Get the value property of the input elements
-      var inputValueDate = inputElementDate.property("value");
-      var inputValueCity = inputElementCity.property("value").toLowerCase();
-      var inputValueState = inputElementState.property("value").toLowerCase();
-      var inputValueCountry = inputElementCountry.property("value").toLowerCase();
-      var inputValueShape = inputElementShape.property("value").toLowerCase();
+      // var inputValueDate = inputElementDate.property("value");
+      // var inputValueCity = inputElementCity.property("value").toLowerCase();
+      // var inputValueState = inputElementState.property("value").toLowerCase();
+      // var inputValueCountry = inputElementCountry.property("value").toLowerCase();
+      // var inputValueShape = inputElementShape.property("value").toLowerCase();
+
+      searchedData.datetime = inputElementDate.property("value");
+      searchedData.city = inputElementCity.property("value").toLowerCase();
+      searchedData.state = inputElementState.property("value").toLowerCase();
+      searchedData.country = inputElementCountry.property("value").toLowerCase();
+      searchedData.shape = inputElementShape.property("value").toLowerCase();
     
-      console.log(inputValueDate);
-      console.log(inputValueCity);
-      console.log(inputValueState);
-      console.log(inputValueCountry);
-      console.log(inputValueShape);
-      
+  
 
-      filteredData = []
-    
-    //filters the data based on the datetime, city, state, country and shape inputs
-      var filteredData1 = tableData.filter(alien => {
-        
-        if (inputValueDate !="") {
-          filteredData = tableData.filter(alien => alien.datetime === inputValueDate);
-         } 
-         else {
-           return false;
-          }
-        
-        
-        if (inputValueCity !="") {
-          filteredData = tableData.filter(alien => alien.city === inputValueCity);
-         } 
-         else {
-          return false;
-          }
-        
+    console.log(searchedData);
 
-        if (inputValueState !="") {
-          filteredData = tableData.filter(alien => alien.state === inputValueState);
-         } 
-         else {
-          return false;;
-          }
-        
 
-        if (inputValueCountry !="") {
-          filteredData = tableData.filter(alien => alien.country === inputValueCountry);
-         } 
-         else {
-          return false;;
-          }
+    //filters the data object by the searchData
 
-        if (inputValueShape !="") {
-          filteredData = tableData.filter(alien => alien.shape === inputValueShape);
-         } 
-         else {
-          return false;;
-          }
-
-            //appends filtered data to the cleared table
-            filteredData1.forEach((aliens) => {
-              var row = tbody.append("tr");
-              Object.entries(aliens).forEach(([key, value]) => {
-                var cell = row.append("td");
-                cell.text(value);
-              });
-            });
-
+      filteredResult = tableData.filter(function (o) {
+          return Object.keys(searchedData).every(function (k) {
+              return o[k] === searchedData[k];
+          });
       });
-      console.log(filteredData);
+
+
+  
+ console.log(filteredResult);
+
     
       //clears the original table while keeping the header row
       var bodyRef = document.getElementById('ufo-table').getElementsByTagName('tbody')[0]; bodyRef.innerHTML = '';
      
+      filteredResult.forEach((aliens) => {
+        var row = tbody.append("tr");
+        Object.entries(aliens).forEach(([key, value]) => {
+          var cell = row.append("td");
+          cell.text(value);
+        });
+      });
      
-      
       document.getElementById("filter-btn").click();
     }
   });
